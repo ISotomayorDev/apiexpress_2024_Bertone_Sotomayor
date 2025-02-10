da0b16b0602c39850f561c3f5b07285cc2d70563
Lucasbertone02 <bertonelucas885@gmail.com>
2025-02-10 18:07:49 -0300

    Feat: Conexion a DB en MYSQL

--- config/conexionDB.js
+++ config/conexionDB.js
@@ -1,5 +1,5 @@
 // config/conexionDB.js
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
 const conexionDB = () => {
   return new Promise((resolve, reject) => {
@@ -8,18 +8,18 @@ const conexionDB = () => {
       host: process.env.DB_HOST || 'localhost',
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'api_express',
-    });
+      database: process.env.DB_NAME || 'api_express'
+    })
 
     // Intenta conectar a la base de datos
     connection.connect((err) => {
       if (err) {
-        reject(err); // Rechaza la promesa si hay un error
+        reject(err) // Rechaza la promesa si hay un error
       } else {
-        resolve(connection); // Resuelve la promesa con la conexión
+        resolve(connection) // Resuelve la promesa con la conexión
       }
-    });
-  });
-};
+    })
+  })
+}
 
-module.exports = conexionDB; // Exporta la función para conectar a la base de datos
+module.exports = conexionDB // Exporta la función para conectar a la base de datos
--- models/server.js
+++ models/server.js
@@ -1,47 +1,47 @@
 // models/server.js
-const express = require('express');
-const cors = require('cors');
-const conexionDB = require('../config/conexionDB'); // Importa la función de conexión
+const express = require('express')
+const cors = require('cors')
+const conexionDB = require('../config/conexionDB') // Importa la función de conexión
 
 class Server {
-  constructor() {
-    this.app = express();
-    this.port = process.env.PORT || 3000;
+  constructor () {
+    this.app = express()
+    this.port = process.env.PORT || 3000
     // Conectar a la base de datos antes de configurar middlewares y rutas
-    this.conectarDB();
-    this.middleware();
-    this.rutas();
+    this.conectarDB()
+    this.middleware()
+    this.rutas()
   }
 
   // Método para conectar a la base de datos
-  async conectarDB() {
+  async conectarDB () {
     try {
-      const connection = await conexionDB(); // Intenta conectar a la base de datos
-      console.log('Conectado a la base de datos MySQL: api_express');
-      this.connection = connection; // Guarda la conexión en la instancia del servidor (opcional)
+      const connection = await conexionDB() // Intenta conectar a la base de datos
+      console.log('Conectado a la base de datos MySQL: api_express')
+      this.connection = connection // Guarda la conexión en la instancia del servidor (opcional)
     } catch (err) {
-      console.error('Error conectando a la base de datos:', err);
-      process.exit(1); // Termina el proceso con un código de error
+      console.error('Error conectando a la base de datos:', err)
+      process.exit(1) // Termina el proceso con un código de error
     }
   }
 
-  middleware() {
-    this.app.use(cors());
-    this.app.use(express.static('public'));
+  middleware () {
+    this.app.use(cors())
+    this.app.use(express.static('public'))
   }
 
-  rutas() {
-    this.app.use('/api/v1/clima', require('../routes/clima'));
-    this.app.use('/api/v1/geocoding', require('../routes/geocoding'));
-    this.app.use('/api/v1/aire', require('../routes/aire'));
-    this.app.use('/api/v1/pronostico', require('../routes/pronostico'));
+  rutas () {
+    this.app.use('/api/v1/clima', require('../routes/clima'))
+    this.app.use('/api/v1/geocoding', require('../routes/geocoding'))
+    this.app.use('/api/v1/aire', require('../routes/aire'))
+    this.app.use('/api/v1/pronostico', require('../routes/pronostico'))
   }
 
-  listen() {
+  listen () {
     this.app.listen(this.port, () => {
-      console.log(`La API está escuchando en el puerto ${this.port}`);
-    });
+      console.log(`La API está escuchando en el puerto ${this.port}`)
+    })
   }
 }
 
-module.exports = Server;
+module.exports = Server
