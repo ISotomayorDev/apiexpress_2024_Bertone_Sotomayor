30d3ec02ed7c10057a18bf5de32ebe6e481c8e41
Lucasbertone02 <bertonelucas885@gmail.com>
2025-03-24 13:38:17 -0300

    Todo fusionado y listo

--- controllers/aire.js
+++ controllers/aire.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 const AireRepository = require('../repositories/aireRepository')
 
-
 const getPolucionFutura = async (req, res) => {
   const { lon, lat, units = 'metric', lang = 'sp' } = req.query
 
@@ -38,30 +37,30 @@ const getPolucionFutura = async (req, res) => {
 }
 
 const getPolucionAire = async (req, res) => {
-  const { lat, lon, units = 'metric', lang = 'sp' } = req.query;
+  const { lat, lon, units = 'metric', lang = 'sp' } = req.query
 
   if (!lat || !lon) {
-    return res.status(400).json({ error: 'Latitude y Longitud son necesarias.' });
+    return res.status(400).json({ error: 'Latitude y Longitud son necesarias.' })
   }
 
   try {
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&lang=${lang}&units=${units}`);
-    const { data } = response;
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&lang=${lang}&units=${units}`)
+    const { data } = response
 
     // Crear instancia del repositorio y guardar los datos en la base de datos
-    const aireRepository = new AireRepository();
+    const aireRepository = new AireRepository()
 
     await aireRepository.guardarAire({
       nombre_ciudad: `Lat: ${lat}, Lon: ${lon}`,
       data
-    });
+    })
 
     res.status(200).json({
       msg: 'Ok',
       data
-    });
+    })
   } catch (error) {
-    console.error(error);
+    console.error(error)
 
     if (error.response) {
       return res.status(error.response.status).json({
@@ -69,17 +68,17 @@ const getPolucionAire = async (req, res) => {
         msg: 'Error al obtener datos del clima',
         error: error.response.data.message || error.response.statusText,
         statusCode: error.response.status
-      });
+      })
     } else {
       res.status(500).json({
         status: 'error',
         msg: 'Error inesperado al obtener la información',
         error: error.message,
         statusCode: 500
-      });
+      })
     }
   }
-};
+}
 
 const getPolucionAireHistorica = async (req, res) => {
   const { lat, lon, start, end, units = 'metric', lang = 'sp' } = req.query
--- repositories/aireRepository.js
+++ repositories/aireRepository.js
@@ -1,24 +1,24 @@
-const conexionDB = require('../config/conexionDB');
+const conexionDB = require('../config/conexionDB')
 
 class AireRepository {
-  async guardarAire(aireData) {
-    const connection = await conexionDB();
+  async guardarAire (aireData) {
+    const connection = await conexionDB()
 
     const query = `
       INSERT INTO aire (
         nombre_ciudad, calidad_aire, co, no, no2, o3, so2, pm2_5, pm10, nh3
       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+    `
 
     try {
       // Extraer valores del JSON recibido
-      const nombre_ciudad = aireData.nombre_ciudad || 'Desconocido';
-      const calidad_aire = aireData.data.list[0].main.aqi;
-      const componentes = aireData.data.list[0].components;
+      const nombreCiudad = aireData.nombre_ciudad || 'Desconocido'
+      const calidadAire = aireData.data.list[0].main.aqi
+      const componentes = aireData.data.list[0].components
 
       const values = [
-        nombre_ciudad,
-        calidad_aire,
+        nombreCiudad,
+        calidadAire,
         componentes.co,
         componentes.no,
         componentes.no2,
@@ -27,17 +27,17 @@ class AireRepository {
         componentes.pm2_5,
         componentes.pm10,
         componentes.nh3
-      ];
+      ]
 
-      const [result] = await connection.query(query, values);
-      return result;
+      const [result] = await connection.query(query, values)
+      return result
     } catch (err) {
-      console.error('Error al guardar los datos del aire:', err);
-      throw err;
+      console.error('Error al guardar los datos del aire:', err)
+      throw err
     } finally {
-      await connection.end();
+      await connection.end()
     }
   }
 }
 
-module.exports = AireRepository;
+module.exports = AireRepository
